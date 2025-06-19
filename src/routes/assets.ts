5b469e65037bf3a41a593fdc8a66300b9208a7e8
Nikhil Sharma <n89sharma@gmail.com>
2025-06-18 22:54:52 -0400

    Prisma using ESM

--- src/routes/assets.ts
+++ src/routes/assets.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { PrismaClient } from '../../prisma/generated/prisma-client-js';
+import { PrismaClient } from '../generated/prisma/client';
 
 const router = express.Router();
 const prisma = new PrismaClient();
