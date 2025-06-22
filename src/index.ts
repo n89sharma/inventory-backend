9d8cf14b58fc3a5580257350d9fcd1881ad849d3
Nikhil Sharma <n89sharma@gmail.com>
2025-06-22 13:59:43 -0400

    Added CORS and Morgan HTTP request logging

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,21 @@
-import express from 'express';
-import assetRoutes from './routes/assets.js';
+import express from 'express'
+import cors from 'cors'
+import assetRoutes from './routes/assets.js'
+import morgan from 'morgan'
 
 const app = express();
 
 app.use(express.json());
-app.use('/assets', assetRoutes);
+app.use(cors({ origin: "http://localhost:5173" }))
+app.use(morgan('dev')); 
 
 app.get('/', (req, res) => {
   res.send('Inventory API');
-});
+})
+
+app.use('/assets', assetRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-});
+})
