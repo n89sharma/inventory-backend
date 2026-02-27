89d7b9efc68da0220e667aa4a5c3740a4252f12f
Nikhil Sharma <n89sharma@gmail.com>
2026-02-27 12:57:18 -0500

    send ids with constants

--- src/routes/constants.ts
+++ src/routes/constants.ts
@@ -14,13 +14,13 @@ router.get('/', async (req, res) => {
   const invoiceTypes = await prisma.invoiceType.findMany()
 
   res.json({
-    coreFunctions: accessories.map((a) => a.accessory),
-    assetTypes: assetTypes.map((a) => a.asset_type),
-    trackingStatuses: trackingStatuses.map((t) => t.status),
-    availabilityStatuses: availabilityStatuses.map((a) => a.status),
-    technicalStatuses: technicalStatuses.map((t) => t.status),
-    roles: roles.map((r) => r.role),
-    invoiceTypes: invoiceTypes.map((i) => i.type)
+    coreFunctions: accessories,
+    assetTypes: assetTypes,
+    trackingStatuses: trackingStatuses,
+    availabilityStatuses: availabilityStatuses,
+    technicalStatuses: technicalStatuses,
+    roles: roles,
+    invoiceTypes: invoiceTypes
   })
 })
 
